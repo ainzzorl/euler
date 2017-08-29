117d849f9507638e6c0f243331fab2bb3550b6a3
ainzzorl <ainzzorl@gmail.com>
2017-08-29 08:41:05 -0700

    Implement GeneratePrimes

--- genprimes.go
+++ genprimes.go
@@ -1,5 +1,17 @@
 package euler
 
+// GeneratePrimes generates prime numbers.
 func GeneratePrimes(max int) []int {
-	return make([]int, 10)
+	visited := make([]bool, max+1)
+	result := []int{}
+	for i := 2; i <= max; i++ {
+		if visited[i] {
+			continue
+		}
+		result = append(result, i)
+		for j := i * 2; j <= max; j += i {
+			visited[j] = true
+		}
+	}
+	return result
 }
--- genprimes_test.go
+++ genprimes_test.go
@@ -9,6 +9,7 @@ func TestGeneratePrimes(t *testing.T) {
 	doTest(t, 1, []int{})
 	doTest(t, 2, []int{2})
 	doTest(t, 10, []int{2, 3, 5, 7})
+	doTest(t, 11, []int{2, 3, 5, 7, 11})
 }
 
 func doTest(t *testing.T, max int, expected []int) {
